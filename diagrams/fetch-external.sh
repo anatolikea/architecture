6ff8fe4992730a7f3f9391cff6d0607d3da4bdd0
Anatolii Krivchenko <anatolii.krivchenko@ingka.ikea.com>
2023-05-03 23:59:32 +0200

    Make CPS example

--- diagrams/fetch-external.sh
+++ diagrams/fetch-external.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-for service in frontend
+for service in plan pick
 do
   for file in ${service}_system.puml ${service}_components.puml ${service}_relations.puml
   do
-    url="https://raw.githubusercontent.com/anatolikea/${service}/master/resources/diagrams/${file}"
+    url="https://raw.githubusercontent.com/anatolikea/${service}-backend/master/resources/diagrams/${file}"
     echo "$url"
     curl -s --fail -H "Authorization: Bearer $GITHUB_TOKEN" "$url" --output "./diagrams/$file" || echo "$file not found"
   done
