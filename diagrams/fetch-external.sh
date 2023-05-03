5480adfec172b2c3b827395ba4f5da34a0f79230
Anatolii Krivchenko <anatolii.krivchenko@ingka.ikea.com>
2023-05-03 18:24:22 +0200

    Adapt for my account

--- diagrams/fetch-external.sh
+++ diagrams/fetch-external.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-for service in frontend auth cart order goods payment
+for service in frontend
 do
   for file in ${service}_system.puml ${service}_components.puml ${service}_relations.puml
   do
-    url="https://raw.githubusercontent.com/acme-corp-tech/${service}/master/resources/diagrams/${file}"
+    url="https://raw.githubusercontent.com/anatolikea/${service}/master/resources/diagrams/${file}"
     echo "$url"
     curl -s --fail -H "Authorization: Bearer $GITHUB_TOKEN" "$url" --output "./diagrams/$file" || echo "$file not found"
   done
